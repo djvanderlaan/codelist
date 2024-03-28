6c00a3a2cd127e23a0c515bbec2d5fa070f401c5
Jan van der  Laan <dj.vanderlaan@cbs.nl>
2024-03-28 16:11:25 +0100

    Docu for coded

--- R/coded.R
+++ R/coded.R
@@ -1,4 +1,39 @@
-
+#' Coded vector
+#' 
+#' A coded vector is a vector with an associated code list. The values in the
+#' codelist should come from this code list. The values also have an associated
+#' label and optionally additional properties such as a description. See
+#' \code{\link{codelist}} for more information on what should and could be in a
+#' code list. 
+#'
+#' @param x vector to convert to coded vector
+#' @param codelist codelist to associate with the values in \code{x}
+#' @param ... Ignored; used to pass extra arguments to other methods
+#'
+#' @details
+#' When \code{codelist} is omitted in case \code{x} is a factor, a code list is
+#' generated from the factor values. 
+#'
+#' @return
+#' Returns an object of type 'coded'. Except when \code{x} is a factor, \code{x}
+#' keeps classes and attributes assiated with \code{x}. This object is a copy of
+#' \code{x} with a \code{codelist} attribute added. 
+#'
+#' When \code{x} is a factor \code{x} is converted to a character vector when
+#' the codes in the code list are of type character or to numeric when the codes
+#' are numeric. 
+#'
+#' @examples
+#'
+#' x <- coded(c(1,4,2), codelist(data.frame(
+#'   codes = 1:4, labels = letters[1:4])))
+#' print(x)
+#' lab(x)
+#'
+#' x <- coded(factor(letters[1:3]))
+#' print(x)
+#' attr(x, "codelist")
+#'
 #' @export
 coded <- function(x, codelist, ...) {
   UseMethod("coded")
