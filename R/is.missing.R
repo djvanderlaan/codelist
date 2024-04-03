6a2f75068f3680ef8e46e4691c0ffaec58c4b01b
djvanderlaan <jan@ntteloos.nl>
2024-04-03 17:48:56 +0200

    is.missing

---
+++ R/is.missing.R
@@ -0,0 +1,33 @@
+#' Find out which elements of a vector have missing values
+#'
+#' @param x vector for which the missing elements have to be detected.
+#'
+#' @param codelist a \code{\link{codelist}} object or a \code{data.frame} that
+#'   is a valid code list.
+#'
+#' @details
+#' Unlike \code{\link{is.na}} \code{is.missing} will also return \code{TRUE} for
+#' elements of \code{x} whose values are indicated in the code list to be
+#' missing values. For that to work \code{codelist} needs to be a valid
+#' \code{\link{codelist}} with a 'missing' column. This column needs to be
+#' interpretable as a logical vector. When \code{codelist} is missing or does
+#' not contain a 'missing' column the result of \code{is.missing} is the same as
+#' \code{\link{is.na}}.
+#'
+#' @return
+#' Returns a logical vector of the same length as \code{x} with \code{TRUE}
+#' indicating corresponing values in \code{x} that can be considered to be
+#' missing.
+#'
+#' @export
+is.missing <- function(x, codelist = attr(x, "codelist")) {
+  if (is.data.frame(codelist) && hasName(codelist, "missing")) {
+    if (!hasName(codelist, "code")) 
+      stop("Invalid codelist: 'code' column is missing.")
+    missing_codes <- codelist$code[as.logical(codelist$missing)]
+    is.na(x) | (x %in% missing_codes)
+  } else {
+    is.na(x)
+  }
+}
+
---
+++ tests/test_is.missing.R
@@ -0,0 +1,76 @@
+library(codelist)
+source("helpers.R")
+
+cl <- codelist(data.frame(
+      code = c("A", "B", "X"),
+      label = c("A", "B", "X"),
+      missing = c(FALSE, FALSE, TRUE)
+    ))
+
+# Codelist missing
+x <- c("A", "X", NA)
+res <- is.missing(x)
+expect_equal(res, c(FALSE, FALSE, TRUE))
+# Codelist not missing; regular use
+x <- c("A", "X", NA)
+res <- is.missing(x, codelist = cl)
+expect_equal(res, c(FALSE, TRUE, TRUE))
+# Codelist as attribute missing; regular use
+x <- c("A", "X", NA)
+attr(x, "codelist") <- cl
+res <- is.missing(x)
+expect_equal(res, c(FALSE, TRUE, TRUE))
+
+# integer missing
+cl <- data.frame(
+      code = c("A", "B", "X"),
+      label = c("A", "B", "X"),
+      missing = c(0, 0, 1)
+    )
+x <- c("A", "X", NA)
+res <- is.missing(x, codelist = cl)
+expect_equal(res, c(FALSE, TRUE, TRUE))
+
+# No missing codes
+cl <- codelist(data.frame(
+      code = c("A", "B", "X"),
+      label = c("A", "B", "X"),
+      missing = c(0, 0, 0)
+    ))
+x <- c("A", "X", NA)
+res <- is.missing(x, codelist = cl)
+expect_equal(res, c(FALSE, FALSE, TRUE))
+# All missing codes
+cl <- codelist(data.frame(
+      code = c("A", "B", "X"),
+      label = c("A", "B", "X"),
+      missing = c(1, 1, 1)
+    ))
+x <- c("A", "X", NA)
+res <- is.missing(x, codelist = cl)
+expect_equal(res, c(TRUE, TRUE, TRUE))
+
+# Missing missing
+cl <- codelist(data.frame(
+      code = c("A", "B", "X"),
+      label = c("A", "B", "X")
+    ))
+x <- c("A", "X", NA)
+res <- is.missing(x, codelist = cl)
+expect_equal(res, c(FALSE, FALSE, TRUE))
+
+# Empty x
+cl <- codelist(data.frame(
+      code = c("A", "B", "X"),
+      label = c("A", "B", "X"),
+      missing = c(FALSE, FALSE, TRUE)
+    ))
+x <- character(0)
+res <- is.missing(x, codelist = cl)
+expect_equal(res, logical(0))
+
+# No code list
+x <- c("A", "X", NA)
+res <- is.missing(x)
+expect_equal(res, c(FALSE, FALSE, TRUE))
+
